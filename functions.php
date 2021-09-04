c3507323f607d1dee8de62823a9d9ac1e8c01d81
Louis-develops <41618495+Louis-develops@users.noreply.github.com>
2021-09-04 16:27:11 +0100

    Further refactored code
    Added and includes directory which holds 'header.php' and 'footer.php'.

--- functions.php
+++ functions.php
@@ -2,6 +2,124 @@
 
 	include "db_connection.php";
 
+
+
+	// delete db record
+	function delete_record(){
+		global $connection;
+		if(isset($_POST["submit"])){
+			$id = $_POST["id"];
+
+			$sql_query = "DELETE FROM products ";
+			$sql_query .= "WHERE ID = $id ";
+
+			$result = mysqli_query($connection, $sql_query);
+
+			if(!$result){
+				die("Error deleting item from database" . mysqli_error($connection));
+			} else {
+				echo "Record deleted";
+			}
+		}
+	}
+	// Update db record
+	function update(){
+		global $connection;
+
+		if(isset($_POST["submit"])){
+			$id = $_POST["id"];
+			$item_name = $_POST["item-name"];
+			$item_price = $_POST["item-price"];
+			$item_category = $_POST["item-category"];
+			$stock_quantity = $_POST["item-stock-quantity"];
+
+			$sql_query = "UPDATE products SET ";
+			$sql_query .= "item_name = '$item_name', ";
+			$sql_query .= "item_price = '$item_price', ";
+			$sql_query .= "item_category = '$item_category', ";
+			$sql_query .= "stock_quantity = $stock_quantity ";
+			$sql_query .= "WHERE ID = $id";
+
+			$result = mysqli_query($connection, $sql_query);
+
+			if(!$result){
+				die("Error sending data to database" . mysqli_error($connection));
+			} else {
+				echo "Item updated";
+			}
+
+		}
+	}
+
+	// Simple read
+	function read(){
+		global $connection;
+
+		$sql_query = "SELECT * FROM products";
+	
+		$result = mysqli_query($connection, $sql_query);
+
+		while($row = mysqli_fetch_assoc($result)){
+			$id = $row["ID"];
+
+			echo "<option>$id</option>";
+		}
+
+	}
+
+	// Read from database
+	function read_db($list_type){
+		global $connection;
+	
+
+		$sql_query = "SELECT * FROM products";
+	
+		$result = mysqli_query($connection, $sql_query);
+
+		if(!$result){
+			die("Error retrieving data" . mysqli_error($connection));
+		} else {
+			while($row = mysqli_fetch_assoc($result)){
+	
+				$id = $row["ID"];
+				$item_name = $row["item_name"];
+				$item_price = $row["item_price"];
+				$item_category = $row["item_category"];
+
+				if($list_type == "li"){
+					echo "<li>PRODUCT_ID: $id PRODUCT_NAME: $item_name PRODUCT_PRICE: $item_price PRODUCT_CATEGORY: $item_category </li>";
+
+				} elseif ($list_type == "option"){
+					echo "<option>$id</option>";
+				} else {
+					echo "<option>Neither</option>";
+				}
+
+			}
+		}
+			
+	}
+
+	// Get column names
+	function get_column_names(){
+		global $connection;
+
+		$sql_query = "SHOW COLUMNS FROM products;";
+
+		$result = mysqli_query($connection, $sql_query);
+
+		if(!$result){
+			echo die("Could not retrieve data from database" . mysqli_error($connection));
+		} else {
+
+			while($row = mysqli_fetch_assoc($result)){
+				echo "<option>" . $row['Field'] . "</option>";
+			}
+		}
+
+	}
+
+
 	// Create store item
 	function create_item(){
 		global $connection;
--- shop_form_update.php
+++ shop_form_update.php
@@ -1,43 +1,27 @@
-<?php include "functions.php"; ?>
+<?php include "includes/header.php"; ?>
 
+	<form class="form" action="shop_form_update.php" method="post">
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Shop Form Update</title>
-</head>
-<body>
+		<label for="item-name">Item Name:</label><br>
+		<input type="text" name="item-name" id="item-name"><br><br>
 
+		<label for="item-price">Item Price:</label><br>
+		<input type="text" name="item-price" id="item-price"><br><br>
 
-<form class="form" action="shop_form_update.php" method="post">
+		<label for="item-category">Item Category:</label><br>
+		<input type="text" name="item-category" id="item-category"><br><br>
 
-<label for="item-name">Item Name:</label><br>
-<input type="text" name="item-name" id="item-name"><br><br>
+		<label for="item-stock-quantity">Item Stock qunatity:</label><br>
+		<input type="number" name="item-stock-quantity" id="item-stock-quantity"><br><br>
 
-<label for="item-price">Item Price:</label><br>
-<input type="text" name="item-price" id="item-price"><br><br>
+		<select name="id" id="">
+			<?php read_db("option"); ?>
+		</select>
 
-<label for="item-category">Item Category:</label><br>
-<input type="text" name="item-category" id="item-category"><br><br>
+		<input type="submit" value="Submit" name="submit">
 
-<label for="item-stock-quantity">Item Stock qunatity:</label><br>
-<input type="number" name="item-stock-quantity" id="item-stock-quantity"><br><br>
+	</form>
 
-<select name="id" id="">
-	<?php read_db("option"); ?>
-</select>
+	<?php update(); ?>
 
-<input type="submit" value="Submit" name="submit">
-
-</form>
-
-<?php update(); ?>
-
-	
-
-	
-
-</body>
-</html>
+<?php include "includes/footer.php"; ?>
